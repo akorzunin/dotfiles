e2608f01b498b498a4f4adff6335bb4aebccc634
Alexey Korzunin <54314123+akorzunin@users.noreply.github.com>
2025-05-20 13:54:20 +0300

    Create ascii_art.ps1

---
+++ nt_config/ascii_art.ps1
@@ -0,0 +1,38 @@
+$ESC = [char]27
+$palettes = @{
+    # 16-color palettes
+    "Rainbow"   = 91,93,92,96,94,95        # Classic bright rainbow
+    "Sunset"    = 91,93,95,53,55,97        # Red -> Yellow -> Pink -> Purple
+    "Ocean"     = 94,96,36,39,45,51         # Blue -> Cyan gradient
+    "Forest"    = 92,106,28,22,58,64       # Green gradient
+    "Voltron"   = 91,93,92,96,94,95,97    # Bright pop-art style
+}
+
+# Select a palette (change this value to try different ones)
+$colors = $palettes["Rainbow"]
+
+$asciiArt = @'
+⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕
+⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕
+⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱
+⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀
+⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗
+⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕
+⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵
+⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿
+⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁
+'@ -split "`n"
+
+$colorIndex = 0
+foreach ($line in $asciiArt) {
+    $coloredLine = ""
+    $colorIndex++
+    foreach ($char in $line.ToCharArray()) {
+        $color = $colors[$colorIndex % $colors.Count]
+        $coloredLine += "${ESC}[${color}m$char"
+        $colorIndex++
+    }
+    $coloredLine += "${ESC}[0m"
+    Write-Host $coloredLine
+}
